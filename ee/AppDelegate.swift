a79f137ed47d77fd3fe1592824c1411d15ab5694
Admin <nkhs@yandex.com>
2018-11-01 20:38:24 +0300

    Initial Commit

---
+++ ee/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ee
+//
+//  Created by Admin on 11/1/18.
+//  Copyright © 2018 Admin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ee/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ee
+//
+//  Created by Admin on 11/1/18.
+//  Copyright © 2018 Admin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
